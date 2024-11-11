d23f0c90f7172c6346df69d0e439533f81c31cba
Ángel Garrón <angelgarron95@hotmail.com>
2024-11-11 13:34:36 +0000

    test_get_time_delay_indices: added test for another sky position

--- tests/test_time_delay.py
+++ tests/test_time_delay.py
@@ -25,7 +25,10 @@ def test_get_time_slides_seconds():
 
 def test_get_time_delay_indices():
     sample_rate = 4096.0
-    time_delay_zerolag_seconds = {0: {"A": 0.25, "B": 0.26, "C": 0.27}}
+    time_delay_zerolag_seconds = {
+        0: {"A": 0.25, "B": 0.26, "C": 0.27},
+        1: {"A": 0.28, "B": 0.29, "C": 0.3},
+    }
     slide_ids = np.array([0, 1, 2, 3])
     time_slides_seconds = {
         "A": np.array([1, 2, 3, 9]),
@@ -44,28 +47,48 @@ def test_get_time_delay_indices():
                 "A": round(((0.25 + 1) * sample_rate)),
                 "B": round(((0.26) * sample_rate)),
                 "C": round(((0.27 + 7) * sample_rate)),
-            }
+            },
+            1: {
+                "A": round(((0.28 + 1) * sample_rate)),
+                "B": round(((0.29) * sample_rate)),
+                "C": round(((0.3 + 7) * sample_rate)),
+            },
         },
         1: {
             0: {
                 "A": round(((0.25 + 2) * sample_rate)),
                 "B": round(((0.26 + 4) * sample_rate)),
                 "C": round(((0.27 + 5) * sample_rate)),
-            }
+            },
+            1: {
+                "A": round(((0.28 + 2) * sample_rate)),
+                "B": round(((0.29 + 4) * sample_rate)),
+                "C": round(((0.3 + 5) * sample_rate)),
+            },
         },
         2: {
             0: {
                 "A": round(((0.25 + 3) * sample_rate)),
                 "B": round(((0.26) * sample_rate)),
                 "C": round(((0.27) * sample_rate)),
-            }
+            },
+            1: {
+                "A": round(((0.28 + 3) * sample_rate)),
+                "B": round(((0.29) * sample_rate)),
+                "C": round(((0.3) * sample_rate)),
+            },
         },
         3: {
             0: {
                 "A": round(((0.25 + 9) * sample_rate)),
                 "B": round(((0.26 + 2) * sample_rate)),
                 "C": round(((0.27 + 43) * sample_rate)),
-            }
+            },
+            1: {
+                "A": round(((0.28 + 9) * sample_rate)),
+                "B": round(((0.29 + 2) * sample_rate)),
+                "C": round(((0.3 + 43) * sample_rate)),
+            },
         },
     }
     np.testing.assert_equal(time_delay_idx, expected_time_delay_idx)

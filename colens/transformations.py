8e9e0bddcd6276f0eb70121c9617f1bc09589920
Ángel Garrón <angelgarron95@hotmail.com>
2025-02-03 11:22:28 +0100

    added ra_to_longitude and longitude_to_ra transformations

--- colens/transformations.py
+++ colens/transformations.py
@@ -1,6 +1,7 @@
 """Functions for performing coordinate transformations."""
 
 import numpy as np
+from pycbc.detector import gmst_accurate
 
 
 def spher_to_cart(sky_points):
@@ -18,3 +19,35 @@ def cart_to_spher(sky_points):
     spher[:, 0] = np.arctan2(sky_points[:, 1], sky_points[:, 0])
     spher[:, 1] = np.arcsin(sky_points[:, 2])
     return spher
+
+
+def ra_to_longitude(
+    ra: float | np.ndarray, gps_time: float | np.ndarray
+) -> float | np.ndarray:
+    """Convert from right ascension to longitude at a given time.
+
+    Args:
+        ra (float | np.ndarray): Right ascension in the range [0, 2*np.pi).
+        gps_time (float | np.ndarray): GPS time when the longitude will be computed.
+
+    Returns:
+        float | np.ndarray: Longitude.
+    """
+    longitude = ra - gmst_accurate(gps_time)
+    return longitude
+
+
+def longitude_to_ra(
+    longitude: float | np.ndarray, gps_time: float | np.ndarray
+) -> float | np.ndarray:
+    """Convert from longitude to right ascension at a given time.
+
+    Args:
+        longitude (float | np.ndarray): Longitude.
+        gps_time (float | np.ndarray): GPS time when the right ascension will be computed.
+
+    Returns:
+        float | np.ndarray: Right ascension.
+    """
+    ra = gmst_accurate(gps_time) + longitude
+    return ra

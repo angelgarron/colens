9569c967e7e028ae7a18df18b660219a15bd69d7
Ángel Garrón <angelgarron95@hotmail.com>
2025-02-11 16:59:22 +0100

    added physical_parameters_to_a

--- colens/recover_parameters.py
+++ colens/recover_parameters.py
@@ -1,4 +1,18 @@
+from dataclasses import dataclass
+
 import numpy as np
+
+D0 = 1
+
+
+@dataclass
+class AmplitudeParameters:
+    A_1: float
+    A_2: float
+    A_3: float
+    A_4: float
+    A_p: float
+    A_c: float
 
 
 def get_A_parameters(sigma, fp, fc, snr_array_at_trigger, instruments):
@@ -47,3 +61,21 @@ def recover_parameters(A1, A2, A3, A4):
         "Ap": Ap,
         "Ac": Ac,
     }
+
+
+def physical_parameters_to_a(distance, iota, psi, phi):
+    A_p = D0 / distance * (1 + np.cos(iota) ** 2) / 2
+    A_c = D0 / distance * np.cos(iota)
+    A_1 = A_p * np.cos(2 * phi) * np.cos(2 * psi) - A_c * np.sin(2 * phi) * np.sin(
+        2 * psi
+    )
+    A_2 = A_p * np.cos(2 * phi) * np.sin(2 * psi) + A_c * np.sin(2 * phi) * np.cos(
+        2 * psi
+    )
+    A_3 = -A_p * np.sin(2 * phi) * np.cos(2 * psi) - A_c * np.cos(2 * phi) * np.sin(
+        2 * psi
+    )
+    A_4 = -A_p * np.sin(2 * phi) * np.sin(2 * psi) + A_c * np.cos(2 * phi) * np.cos(
+        2 * psi
+    )
+    return AmplitudeParameters(A_1, A_2, A_3, A_4, A_p, A_c)

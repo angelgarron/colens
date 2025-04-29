e6cb6d67c328e7544231cba09e4443c7d78246ed
Ángel Garrón <angelgarron95@hotmail.com>
2025-04-29 14:57:54 +0200

    io.py: removed create_filter_bank function

--- colens/io.py
+++ colens/io.py
@@ -64,38 +64,6 @@ def get_strain_dict_from_files(
     return strain_dict
 
 
-def create_filter_bank(
-    mass1: float,
-    mass2: float,
-    spin1z: float,
-    spin2z: float,
-    bank_file: str,
-    reference_frequency: float,
-) -> None:
-    """Create a file with the template bank.
-
-    Args:
-        mass1 (float): Parameter.
-        mass2 (float): Parameter.
-        spin1z (float): Parameter.
-        spin2z (float): Parameter.
-        bank_file (str): File name where to store the template bank.
-        reference_frequency (float): Reference frequency.
-    """
-    with h5py.File(bank_file, "w") as file:
-        for key, value in {
-            "mass1": mass1,
-            "mass2": mass2,
-            "spin1z": spin1z,
-            "spin2z": spin2z,
-            "f_final": 2048.0,
-            "f_ref": reference_frequency,
-        }.items():
-            file.create_dataset(
-                key, data=[value], compression="gzip", compression_opts=9, shuffle=True
-            )
-
-
 def get_bilby_posteriors(filename: Path | str) -> pd.DataFrame:
     with h5py.File(filename) as file:
         return pd.DataFrame(np.array(file["C01:IMRPhenomXPHM"]["posterior_samples"]))
